a42411a2adeef2b68d218f283e385493dc2298bf
Doreenmoraa2 <doreenmoraa2019@gmail.com>
2022-01-28 16:56:20 +0300

    Palindrome-js

---
+++ index.js
@@ -0,0 +1,11 @@
+let x="racecar";
+
+y=x.split("").reverse().join("");
+if (y == x) {
+    console.log("This is a palindrome");
+}
+else{
+    console.log("This is not a palindrome");
+}
+
+console.log(y)
